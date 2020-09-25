350f641e702817ceea36c2331d8379b2dbc944c5
SmokedDuck1591 <coutinho159123@gmail.com>
2020-09-25 23:00:24 +0900

    현재까지 한 거

--- clock.js
+++ clock.js
@@ -1,4 +1,4 @@
-const clockContainer = document.querySelector(".clock");
+const clockContainer = document.querySelector(".js-clock");
 const clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
